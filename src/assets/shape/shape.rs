47921d7a8e460bc439ce0bc5d0c5e82227f506fa
Brendan <brendansbortz@protonmail.com>
2022-09-08 08:36:26 -0400

    better

--- src/assets/shape/shape.rs
+++ src/assets/shape/shape.rs
@@ -1,4 +1,5 @@
 use super::vertex::Vertex;
+use cgmath::{Vector2, Zero};
 use glium::{index::PrimitiveType, Display, IndexBuffer, VertexBuffer};
 use std::{cell::RefCell, rc::Rc};
 
@@ -9,13 +10,29 @@ pub struct Shape {
 
 impl Shape {
     pub fn new(
-        display: &Display,
         vertices: &[Vertex],
         indices: &[u32],
+        display: &Display,
     ) -> anyhow::Result<Rc<RefCell<Self>>> {
         Ok(Rc::new(RefCell::new(Self {
             vertices: VertexBuffer::new(display, vertices)?,
             indices: IndexBuffer::immutable(display, PrimitiveType::TrianglesList, &indices)?,
         })))
+    }
+
+    pub fn new_rect(dims: Vector2<f32>, display: &Display) -> anyhow::Result<Rc<RefCell<Self>>> {
+        let indices = [0, 1, 2, 1, 3, 2];
+        let vertices = {
+            let dims = dims / 2.0;
+
+            [
+                Vertex::new(Vector2::new(-dims.x, -dims.y), Vector2::zero()),
+                Vertex::new(Vector2::new(-dims.x, dims.y), Vector2::new(0.0, 1.0)),
+                Vertex::new(Vector2::new(dims.x, -dims.y), Vector2::new(1.0, 0.0)),
+                Vertex::new(Vector2::new(dims.x, dims.y), Vector2::new(1.0, 1.0)),
+            ]
+        };
+
+        Self::new(&vertices, &indices, display)
     }
 }
