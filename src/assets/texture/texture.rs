fc5693434c5df2b2e4db4b9effa16de350cdb5f7
Brendan <brendansbortz@protonmail.com>
2022-09-08 13:02:49 -0400

    better

--- src/assets/texture/texture.rs
+++ src/assets/texture/texture.rs
@@ -5,34 +5,31 @@ use glium::{
 };
 use std::{cell::RefCell, rc::Rc};
 
-pub struct Texture<'a, F>
-where
-    F: FnMut() -> RawImage2d<'a, u8>,
-{
-    pub process: F,
+pub struct Texture<'a> {
+    pub image: RawImage2d<'a, u8>,
     pub mipmaps_option: MipmapsOption,
 }
 
-impl<'a, F> Texture<'a, F>
-where
-    F: FnMut() -> RawImage2d<'a, u8>,
-{
-    pub fn new(process: F, mipmaps_option: MipmapsOption) -> anyhow::Result<Rc<RefCell<Self>>> {
+impl<'a> Texture<'a> {
+    pub fn new(
+        image: RawImage2d<'a, u8>,
+        mipmaps_option: MipmapsOption,
+    ) -> anyhow::Result<Rc<RefCell<Self>>> {
         Ok(Rc::new(RefCell::new(Self {
-            process,
+            image,
             mipmaps_option,
         })))
     }
 }
 
-impl<'a, F> TextureBuffer for Texture<'a, F>
-where
-    F: FnMut() -> RawImage2d<'a, u8>,
-{
+impl<'a> TextureBuffer for Texture<'a> {
     fn bind(&mut self, display: &Display) -> anyhow::Result<SrgbTexture2d> {
         Ok(SrgbTexture2d::with_mipmaps(
             display,
-            (self.process)(),
+            RawImage2d {
+                data: self.image.data.clone(),
+                ..self.image
+            },
             self.mipmaps_option,
         )?)
     }
