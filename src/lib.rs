7ca243f2e9b4295c1f1256e78edf842348bcf506
Brendan <brendansbortz@protonmail.com>
2022-09-22 09:50:06 -0400

    better

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-pub use anyhow;
-pub use cgmath;
-pub use glium;
-pub use hecs as ecs;
-
 pub mod assets;
 pub mod components;
 pub mod engine;
 pub mod systems;
+
+pub use anyhow;
+pub use cgmath;
+pub use glium;
+pub use hecs as ecs;
