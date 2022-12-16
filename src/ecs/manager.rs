6abee4f27afe664e166af2d42650f347c6d718d7
Brendan Bortz <brendansbortz@protonmail.com>
2022-12-16 09:15:55 -0500

    ran clippy

--- src/ecs/manager.rs
+++ src/ecs/manager.rs
@@ -1,4 +1,4 @@
-use super::{cast, cast_mut, cast_ref, AsAny, Component};
+use super::{cast_mut, cast_ref, AsAny, Component};
 use std::collections::HashMap;
 
 #[derive(Default)]
@@ -8,39 +8,37 @@ pub struct Manager<'a> {
 }
 
 impl<'a> Manager<'a> {
-    pub fn add_c_generic(&mut self, eid: usize, cid: usize, component: Box<dyn AsAny<'a>>) {
+    pub fn add_c_generic(&mut self, eid: usize, cid: usize, component: Box<dyn AsAny<'a>>) -> Option<usize> {
         let id = self.cache.len();
 
         self.cache.push(component);
-
-        self.components.get_mut(&eid).map(|c| c.insert(cid, id));
+        self.components.get_mut(&eid).map(|c| c.insert(cid, id)).map(|_| id)
     }
 
-    pub fn add_c<C>(&mut self, eid: usize, component: C)
+    pub fn add_c<C>(&mut self, eid: usize, component: C) -> Option<usize>
     where
         C: Component + 'a,
     {
-        self.add_c_generic(eid, C::id(), Box::new(component));
+        self.add_c_generic(eid, C::id(), Box::new(component))
     }
 
-    pub fn rm_c_generic(&mut self, eid: usize, cid: usize) -> Option<Box<dyn AsAny<'a>>> {
-        self.components
-            .get_mut(&eid)
-            .and_then(|c| c.remove(&cid))
-            .map(|c| self.cache.remove(c))
+    pub fn rm_c_generic(&mut self, eid: usize, cid: usize) {
+        if let Some(c) = self.components.get_mut(&eid).and_then(|c| c.remove(&cid)) {
+            self.cache.remove(c);
+        }
     }
 
-    pub fn rm_c<C>(&mut self, eid: usize) -> Option<C>
+    pub fn rm_c<C>(&mut self, eid: usize)
     where
         C: Component,
     {
-        self.rm_c_generic(eid, C::id()).map(|c| cast(c))
+        self.rm_c_generic(eid, C::id());
     }
 
-    pub fn get_c_generic(&self, eid: usize, cid: usize) -> Option<&Box<dyn AsAny<'a>>> {
+    pub fn get_c_generic(&self, eid: usize, cid: usize) -> Option<&dyn AsAny<'a>> {
         self.components
             .get(&eid)
-            .and_then(|c| c.get(&cid).map(|c| &self.cache[*c]))
+            .and_then(|c| c.get(&cid).map(|c| &*self.cache[*c]))
     }
 
     pub fn get_c<C>(&self, eid: usize) -> Option<&C>
@@ -82,11 +80,11 @@ impl<'a> Manager<'a> {
     }
 
     pub fn rm_e(&mut self, eid: usize) {
-        self.components.remove(&eid).map(|e| {
+        if let Some(e) = self.components.remove(&eid) {
             for v in e.values() {
                 self.rm_c_generic(eid, *v);
             }
-        });
+        }
     }
 
     pub fn entities(&self) -> Vec<usize> {
