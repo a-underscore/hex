553220ffc7748f40a9291b6ce82680fd2bb1f947
Brendan Bortz <brendansbortz@protonmail.com>
2022-12-17 12:55:03 -0500

    b

--- src/ecs/manager.rs
+++ src/ecs/manager.rs
@@ -3,8 +3,9 @@ use std::collections::HashMap;
 
 #[derive(Default)]
 pub struct Manager<'a> {
-    pub components: HashMap<usize, HashMap<usize, usize>>,
-    pub cache: Vec<Box<dyn AsAny<'a>>>,
+    pub entities: HashMap<usize, HashMap<usize, usize>>,
+    pub cache: HashMap<usize, Box<dyn AsAny<'a>>>,
+    pub freed: Vec<usize>,
 }
 
 impl<'a> Manager<'a> {
@@ -14,10 +15,10 @@ impl<'a> Manager<'a> {
         cid: usize,
         component: Box<dyn AsAny<'a>>,
     ) -> Option<usize> {
-        let id = self.cache.len();
+        let id = self.freed.pop().unwrap_or(self.cache.len());
 
-        self.components.get_mut(&eid).map(|c| c.insert(cid, id))?;
-        self.cache.push(component);
+        self.entities.get_mut(&eid).map(|c| c.insert(cid, id))?;
+        self.cache.insert(id, component);
 
         Some(id)
     }
@@ -30,8 +31,9 @@ impl<'a> Manager<'a> {
     }
 
     pub fn rm_c_gen(&mut self, eid: usize, cid: usize) {
-        if let Some(c) = self.components.get_mut(&eid).and_then(|c| c.remove(&cid)) {
-            self.cache.remove(c);
+        if let Some(c) = self.entities.get_mut(&eid).and_then(|c| c.remove(&cid)) {
+            self.cache.remove(&c);
+            self.freed.push(c);
         }
     }
 
@@ -43,7 +45,7 @@ impl<'a> Manager<'a> {
     }
 
     pub fn get_c_gen(&self, eid: usize, cid: usize) -> Option<&dyn AsAny<'a>> {
-        self.components
+        self.entities
             .get(&eid)
             .and_then(|c| c.get(&cid).copied())
             .and_then(|cid| self.get_c_gen_cached(cid))
@@ -57,7 +59,7 @@ impl<'a> Manager<'a> {
     }
 
     pub fn get_c_gen_mut(&mut self, eid: usize, cid: usize) -> Option<&mut dyn AsAny<'a>> {
-        self.components
+        self.entities
             .get_mut(&eid)
             .and_then(|c| c.get_mut(&cid).map(|cid| *cid))
             .and_then(|cid| self.get_c_gen_cached_mut(cid))
@@ -71,7 +73,7 @@ impl<'a> Manager<'a> {
     }
 
     pub fn get_c_gen_cached(&self, cid: usize) -> Option<&dyn AsAny<'a>> {
-        self.cache.get(cid).map(|c| c.as_ref())
+        self.cache.get(&cid).map(|c| c.as_ref())
     }
 
     pub fn get_c_cached<C>(&self, cid: usize) -> Option<&C>
@@ -82,7 +84,7 @@ impl<'a> Manager<'a> {
     }
 
     pub fn get_c_gen_cached_mut(&mut self, cid: usize) -> Option<&mut dyn AsAny<'a>> {
-        self.cache.get_mut(cid).map(|c| c.as_mut())
+        self.cache.get_mut(&cid).map(|c| c.as_mut())
     }
 
     pub fn get_c_cached_mut<C>(&mut self, cid: usize) -> Option<&mut C>
@@ -113,26 +115,27 @@ impl<'a> Manager<'a> {
     pub fn add_e(&mut self, eid: usize) {
         self.rm_e(eid);
 
-        self.components.insert(eid, HashMap::new());
+        self.entities.insert(eid, HashMap::new());
     }
 
     pub fn get_e(&self, eid: usize) -> Option<&HashMap<usize, usize>> {
-        self.components.get(&eid)
+        self.entities.get(&eid)
     }
 
     pub fn get_e_mut(&mut self, eid: usize) -> Option<&mut HashMap<usize, usize>> {
-        self.components.get_mut(&eid)
+        self.entities.get_mut(&eid)
     }
 
     pub fn rm_e(&mut self, eid: usize) {
-        if let Some(e) = self.components.remove(&eid) {
+        if let Some(e) = self.entities.remove(&eid) {
             for v in e.values().cloned() {
-                self.cache.remove(v);
+                self.cache.remove(&v);
+                self.freed.push(v);
             }
         }
     }
 
     pub fn entities(&self) -> Vec<usize> {
-        self.components.keys().cloned().collect()
+        self.entities.keys().cloned().collect()
     }
 }
