4af8ebc4f4494a0c3bd9b3896407844ee7a2aa94
Brendan Bortz <brendansbortz@protonmail.com>
2022-12-18 09:38:37 -0500

    b

--- src/ecs/system.rs
+++ src/ecs/system.rs
@@ -2,10 +2,6 @@ use super::Manager;
 use glium::glutin::event::Event;
 
 pub trait System<'a>: 'a {
-    fn init(&mut self, _: &mut Manager) -> anyhow::Result<()> {
-        Ok(())
-    }
-
     fn update(&mut self, _: &mut Manager, _: &Event<()>) -> anyhow::Result<()> {
         Ok(())
     }
