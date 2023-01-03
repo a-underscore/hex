ef8dab4ff99bed4ce4d94169d878f855de009dd4
Brendan Bortz <brendansbortz@protonmail.com>
2023-01-03 14:09:49 -0500

    z ordering works now

--- src/renderer.rs
+++ src/renderer.rs
@@ -7,6 +7,7 @@ use crate::{
     },
 };
 use glium::{uniform, uniforms::Sampler, Display, Surface};
+use std::cmp::Ordering;
 
 pub struct Renderer {
     pub shader: Shader,
@@ -36,40 +37,50 @@ impl<'a> System<'a> for Renderer {
                         ))
                     })
             }) {
-                for e in world.entity_manager.entities.keys().cloned() {
-                    if let Some((s, t)) = world
-                        .component_manager
-                        .get::<Sprite>(e, &world.entity_manager)
-                        .and_then(|s| {
-                            Some((
-                                s.active.then_some(s)?,
-                                world
-                                    .component_manager
-                                    .get::<Transform>(e, &world.entity_manager)?,
-                            ))
-                        })
-                    {
-                        let color: [f32; 4] = s.color.into();
-                        let transform: [[f32; 3]; 3] = t.matrix().into();
-                        let camera_view: [[f32; 4]; 4] = c.view().into();
-                        let camera_transform: [[f32; 3]; 3] = ct.matrix().into();
-                        let uniform = uniform! {
-                            z: s.z,
-                            transform: transform,
-                            camera_transform: camera_transform,
-                            camera_view: camera_view,
-                            color: color,
-                            image: Sampler(&*s.texture.buffer, s.texture.sampler_behaviour),
-                        };
+                let mut sprites: Vec<_> = world
+                    .entity_manager
+                    .entities
+                    .keys()
+                    .cloned()
+                    .filter_map(|e| {
+                        world
+                            .component_manager
+                            .get::<Sprite>(e, &world.entity_manager)
+                            .and_then(|s| {
+                                Some((
+                                    s.active.then_some(s)?,
+                                    world
+                                        .component_manager
+                                        .get::<Transform>(e, &world.entity_manager)?,
+                                ))
+                            })
+                    })
+                    .collect();
 
-                        target.draw(
-                            &*s.shape.vertices,
-                            &*s.shape.indices,
-                            &self.shader.program,
-                            &uniform,
-                            &s.draw_parameters,
-                        )?;
-                    }
+                sprites
+                    .sort_by(|(s1, _), (s2, _)| s1.z.partial_cmp(&s2.z).unwrap_or(Ordering::Equal));
+
+                for (s, t) in sprites {
+                    let color: [f32; 4] = s.color.into();
+                    let transform: [[f32; 3]; 3] = t.matrix().into();
+                    let camera_view: [[f32; 4]; 4] = c.view().into();
+                    let camera_transform: [[f32; 3]; 3] = ct.matrix().into();
+                    let uniform = uniform! {
+                        z: s.z,
+                        transform: transform,
+                        camera_transform: camera_transform,
+                        camera_view: camera_view,
+                        color: color,
+                        image: Sampler(&*s.texture.buffer, s.texture.sampler_behaviour),
+                    };
+
+                    target.draw(
+                        &*s.shape.vertices,
+                        &*s.shape.indices,
+                        &self.shader.program,
+                        &uniform,
+                        &s.draw_parameters,
+                    )?;
                 }
             }
         }
