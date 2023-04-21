b1f1a8d15585c5806034569b4b34ec1b82ee6942
Brendan Bortz <brendansbortz@protonmail.com>
2023-04-21 08:25:27 -0400

    b

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -1,7 +1,9 @@
 pub mod mat3d;
 pub mod ortho;
 pub mod vec2d;
+pub mod vec3d;
 
 pub use mat3d::Mat3d;
 pub use ortho::Ortho;
 pub use vec2d::Vec2d;
+pub use vec3d::Vec3d;
--- src/math/vec3d.rs
+++ src/math/vec3d.rs
@@ -1,11 +1,11 @@
 use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign};
 
 #[derive(Default, PartialEq, PartialOrd, Copy, Clone)]
-pub struct Vec2d(pub [f32; 2]);
+pub struct Vec3d(pub [f32; 3]);
 
-impl Vec2d {
-    pub fn new(x: f32, y: f32) -> Self {
-        Self([x, y])
+impl Vec3d {
+    pub fn new(x: f32, y: f32, z: f32) -> Self {
+        Self([x, y, z])
     }
 
     pub fn x(self) -> f32 {
@@ -16,12 +16,20 @@ impl Vec2d {
         self.0[1]
     }
 
+    pub fn z(self) -> f32 {
+        self.0[2]
+    }
+
     pub fn x_ref(&self) -> &f32 {
         &self.0[0]
     }
 
     pub fn y_ref(&self) -> &f32 {
         &self.0[1]
+    }
+
+    pub fn z_ref(&self) -> &f32 {
+        &self.0[2]
     }
 
     pub fn x_mut(&mut self) -> &mut f32 {
@@ -32,8 +40,20 @@ impl Vec2d {
         &mut self.0[1]
     }
 
+    pub fn z_mut(&mut self) -> &mut f32 {
+        &mut self.0[2]
+    }
+
     pub fn dot(self, rhs: Self) -> f32 {
-        self.x() * rhs.x() + self.y() * rhs.y()
+        self.x() * rhs.x() + self.y() * rhs.y() + self.z() * rhs.z()
+    }
+
+    pub fn cross(self, rhs: Self) -> Self {
+        Vec3d::new(
+            self.y() * rhs.z() - self.z() * rhs.y(),
+            -(self.x() * rhs.z() - self.z() - rhs.x()),
+            self.x() * rhs.y() - self.y() * rhs.x(),
+        )
     }
 
     pub fn determinant(self, rhs: Self) -> f32 {
@@ -53,35 +73,35 @@ impl Vec2d {
     }
 }
 
-impl Add for Vec2d {
+impl Add for Vec3d {
     type Output = Self;
 
     fn add(self, rhs: Self) -> Self::Output {
-        Self::new(self.x() + rhs.x(), self.y() + rhs.y())
+        Self::new(self.x() + rhs.x(), self.y() + rhs.y(), self.z() + rhs.z())
     }
 }
 
-impl AddAssign for Vec2d {
+impl AddAssign for Vec3d {
     fn add_assign(&mut self, rhs: Self) {
         *self = Self::add(*self, rhs);
     }
 }
 
-impl Mul<f32> for Vec2d {
+impl Mul<f32> for Vec3d {
     type Output = Self;
 
     fn mul(self, rhs: f32) -> Self {
-        Self::new(self.x() * rhs, self.y() * rhs)
+        Self::new(self.x() * rhs, self.y() * rhs, self.z() * rhs)
     }
 }
 
-impl MulAssign<f32> for Vec2d {
+impl MulAssign<f32> for Vec3d {
     fn mul_assign(&mut self, rhs: f32) {
         *self = Self::mul(*self, rhs);
     }
 }
 
-impl Neg for Vec2d {
+impl Neg for Vec3d {
     type Output = Self;
 
     fn neg(self) -> Self::Output {
@@ -89,7 +109,7 @@ impl Neg for Vec2d {
     }
 }
 
-impl Sub for Vec2d {
+impl Sub for Vec3d {
     type Output = Self;
 
     fn sub(self, rhs: Self) -> Self {
@@ -97,13 +117,13 @@ impl Sub for Vec2d {
     }
 }
 
-impl SubAssign for Vec2d {
+impl SubAssign for Vec3d {
     fn sub_assign(&mut self, rhs: Self) {
         *self = Self::sub(*self, rhs);
     }
 }
 
-impl Div<f32> for Vec2d {
+impl Div<f32> for Vec3d {
     type Output = Self;
 
     fn div(self, rhs: f32) -> Self {
@@ -111,7 +131,7 @@ impl Div<f32> for Vec2d {
     }
 }
 
-impl DivAssign<f32> for Vec2d {
+impl DivAssign<f32> for Vec3d {
     fn div_assign(&mut self, rhs: f32) {
         *self = Self::div(*self, rhs);
     }
