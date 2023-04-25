ee9b83edf3e0d8899242d4990c4a1ee327a2f3c8
Brendan Bortz <brendansbortz@protonmail.com>
2023-04-25 13:17:12 -0400

    b

--- src/systems/light_renderer/fragment.glsl
+++ src/systems/light_renderer/fragment.glsl
@@ -6,12 +6,12 @@ in vec3 v_normal;
 uniform sampler2D buffer;
 uniform vec2 screen_dims;
 uniform vec3 light_color;
-uniform vec3 light_pos;
+uniform vec3 light_position;
 uniform float light_strength;
 
 void main(void) {
 	vec3 ambient = light_strength * light_color;
-	vec3 light_dir = normalize(light_pos - v_pos);
+	vec3 light_dir = normalize(light_position - v_pos);
 	vec3 diffuse = max(dot(v_normal, light_dir), 0.0) * light_color;
 	vec4 texture = texture(buffer, gl_FragCoord.xy / screen_dims);
 
