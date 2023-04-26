1e00e55d81afc4036467f7a10076167bfdcf8f4c
Brendan Bortz <brendansbortz@protonmail.com>
2023-04-26 15:25:09 -0400

    b

--- src/renderer/vertex.glsl
+++ src/renderer/vertex.glsl
@@ -8,12 +8,12 @@ out vec2 tex_pos;
 uniform float z;
 uniform mat3 transform;
 uniform mat3 camera_transform;
-uniform mat4 camera_view;
+uniform mat4 camera_proj;
 
 void main(void) {
         vec2 pos = (vec3(position, 1.0) * transform * inverse(camera_transform)).xy;
 
-        gl_Position = vec4(vec3(pos, z), 1.0) * camera_view;
+        gl_Position = vec4(vec3(pos, z), 1.0) * camera_proj;
 
 	tex_pos = uv;
 }
