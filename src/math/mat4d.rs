fe1f1bd31d691dbbc0550c78ae120e040da2cd12
Brendan Bortz <brendansbortz@protonmail.com>
2023-04-26 17:11:58 -0400

    checkpoint

--- src/math/mat4d.rs
+++ src/math/mat4d.rs
@@ -84,8 +84,8 @@ impl Mat4d {
 
         Self::new(
             [1.0, 0.0, 0.0, 0.0],
-            [0.0, cos, sin, 0.0],
-            [0.0, -sin, cos, 0.0],
+            [0.0, cos, -sin, 0.0],
+            [0.0, sin, cos, 0.0],
             [0.0, 0.0, 0.0, 1.0],
         )
     }
@@ -94,9 +94,9 @@ impl Mat4d {
         let (sin, cos) = rotation.sin_cos();
 
         Self::new(
-            [cos, 0.0, -sin, 0.0],
+            [cos, 0.0, sin, 0.0],
             [0.0, 1.0, 0.0, 0.0],
-            [sin, 0.0, cos, 0.0],
+            [-sin, 0.0, cos, 0.0],
             [0.0, 0.0, 0.0, 1.0],
         )
     }
@@ -105,8 +105,8 @@ impl Mat4d {
         let (sin, cos) = rotation.sin_cos();
 
         Self::new(
-            [cos, sin, 0.0, 0.0],
-            [-sin, cos, 0.0, 0.0],
+            [cos, -sin, 0.0, 0.0],
+            [sin, cos, 0.0, 0.0],
             [0.0, 0.0, 1.0, 0.0],
             [0.0, 0.0, 0.0, 1.0],
         )
