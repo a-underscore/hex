7a5d99252f4f957c17fb241bfc50d4c878e15994
Brendan Bortz <brendansbortz@protonmail.com>
2023-09-28 15:05:19 -0400

    b

--- src/ecs/entity_manager.rs
+++ src/ecs/entity_manager.rs
@@ -29,8 +29,8 @@ impl EntityManager {
 
     pub fn rm(&mut self, eid: Id, cm: &mut ComponentManager) {
         if let Some(e) = self.entities.remove(&eid) {
-            for cid in e.keys().cloned() {
-                cm.rm_gen(eid, cid, self);
+            for cid in e.values().cloned() {
+                cm.cache.remove(&cid);
             }
         }
     }
