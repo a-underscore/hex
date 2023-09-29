76a60ce43e4fee579ca8f0a9fa80af648eec4577
Brendan Bortz <brendansbortz@protonmail.com>
2023-09-29 00:34:38 -0400

    b

--- src/lib.rs
+++ src/lib.rs
@@ -8,17 +8,3 @@ pub use anyhow;
 pub use glium;
 pub use once_cell;
 pub use renderer::Renderer;
-
-#[macro_export]
-macro_rules! id {
-    () => {{
-        use $crate::{
-            ecs::{id, Id},
-            once_cell::sync::Lazy,
-        };
-
-        static ID: Lazy<Id> = Lazy::new(|| id());
-
-        *ID
-    }};
-}
