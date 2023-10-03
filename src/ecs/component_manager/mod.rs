b756451842ac21efe7f160c4aeac208636ee9b4b
Brendan Bortz <brendansbortz@protonmail.com>
2023-10-02 22:02:34 -0400

    b

--- src/ecs/component_manager/mod.rs
+++ src/ecs/component_manager/mod.rs
@@ -47,11 +47,11 @@ impl ComponentManager {
 
     pub fn rm_gen(&mut self, eid: Id, cid: TypeId, em: &mut EntityManager) {
         if let Some(id) = em.components.remove(&(eid, cid)) {
-            self.rm_cache(id);
-
             if let Some(components) = em.entities.get_mut(&eid) {
                 components.remove(&cid);
             }
+
+            self.rm_cache(id);
         }
     }
 
