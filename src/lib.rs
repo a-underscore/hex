db50e27d011c72855d5c9fe4f64ab328f7f279f7
Brendan Bortz <brendansbortz@protonmail.com>
2023-11-22 14:58:35 -0500

    b

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,9 @@ pub mod assets;
 pub mod components;
 pub mod ecs;
 pub mod math;
+pub mod sprite_renderer;
 
 pub use anyhow;
+pub use sprite_renderer::SpriteRenderer;
 pub use vulkano;
 pub use winit;
