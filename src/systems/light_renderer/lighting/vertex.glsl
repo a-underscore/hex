3a0dbe1a30de05a97402b98f41d4203d1978631e
Brendan Bortz <brendansbortz@protonmail.com>
2023-12-14 00:10:34 -0500

    b

--- src/systems/light_renderer/lighting/vertex.glsl
+++ src/systems/light_renderer/lighting/vertex.glsl
@@ -7,10 +7,16 @@ uniform mat4 transform;
 uniform mat4 camera_transform;
 uniform mat4 camera_proj;
 
+out vec3 v_pos;
+out vec3 v_normal;
+
 void main(void) {
-	mat4 view =  inverse(camera_transform) * transform;
+	mat4 view = inverse(camera_transform) * transform;
 
         vec4 pos = view * vec4(position, 1.0);
 
         gl_Position = camera_proj * pos;
+
+	v_pos = vec3(transform * vec4(position, 1.0));
+	v_normal = normalize(mat3(transpose(inverse(transform))) * normal);
 }
