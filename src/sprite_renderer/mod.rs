e54a0c0648f5aa47b68848a54e027fe172efd0a4
Brendan Bortz <brendansbortz@protonmail.com>
2024-05-14 13:39:49 -0400

    b

--- src/sprite_renderer/mod.rs
+++ src/sprite_renderer/mod.rs
@@ -93,7 +93,7 @@ impl SpriteRenderer {
                     viewports: [Viewport {
                         offset: [0.0, 0.0],
                         extent: [extent[0] as f32, extent[1] as f32],
-                        depth_range: -1.0..=1.0,
+                        depth_range: 0.0..=1.0,
                     }]
                     .into_iter()
                     .collect(),
